9eca2e867aa1863c331c5886cad4c6ce0054620a
Liu-Chen-CS <liu.chenbusiness@gmail.com>
2024-10-09 15:12:14 +0200

    init commit

---
+++ iubh-back/src/main/java/com/liuchen/iubhback/IubhBackApplication.java
@@ -0,0 +1,13 @@
+package com.liuchen.iubhback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IubhBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IubhBackApplication.class, args);
+    }
+
+}
---
+++ iubh-back/src/test/java/com/liuchen/iubhback/IubhBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuchen.iubhback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IubhBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
